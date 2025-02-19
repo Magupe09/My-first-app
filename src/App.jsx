d4732897c4b96410032d9ce300aa6027e74cd679
maonvacation <maonvacation@gmail.com>
2025-02-19 23:29:01 +0000

    "Agrego funcionalidades,eliminar,tachar,y guardar en localStorage"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,61 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
   const [nuevaTarea, setNuevaTarea] = useState("");
   const [tareas, setTareas] = useState([])
+  
+  // Eliminar todas las tareas
+  const eliminarTodasLasTareas=()=>{
+    setTareas([]);
+    localStorage.removeItem("tareas")
+
+  }
+
+
+   // Cargar tareas almacenadas en localStorage al iniciar la app
+  useEffect(() => {
+    const tareasGuardadas = JSON.parse(localStorage.getItem("tareas")) || [];
+    setTareas(tareasGuardadas);
+  }, []);
+
+  // Guardar tareas en localStorage cuando haya cambios
+  useEffect(() => {
+    if (tareas.length === 0){
+      localStorage.removeItem("tareas")
+    }else{
+     localStorage.setItem("tareas", JSON.stringify(tareas));
+    }
+  }, [tareas]);
+  
+    // Eliminar una tarea específica
+
+  const eliminarTarea = (index) => {
+    const nuevasTareas= tareas.filter((_, i) => i !== index);
+    setTareas(nuevasTareas);
+  };
+   
+   // Marcar o desmarcar tarea como completada
+  const marcarComoHecha=(index)=>{
+    const nuevasTareas = [...tareas];
+    nuevasTareas[index].completada = !nuevasTareas[index].completada;
+    setTareas(nuevasTareas);
+  }
+
+  // Manejar el cambio en el input de nueva tarea
 
   const handleInputChange = (e) => {
     setNuevaTarea(e.target.value);    
   };
+
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTareas([...tareas, nuevaTarea]);
+    if(nuevaTarea.trim() === "") return console.log("Por favor ingrese una tarea");
+    setTareas([...tareas,{ texto: nuevaTarea, completada: false }])
+  
     setNuevaTarea("");
-   //console.log(nuevaTarea);
+  
   };
   
   return (
@@ -22,10 +66,19 @@ function App() {
         <button type="submit" >Agregar Tarea</button>
         <ol>
           {tareas.map((tarea, index) => (
-            <li key={index}>{tarea}</li>
+            <li key={index}
+            style={{ textDecoration: tarea.completada ? "line-through" : "none" }}
+            >{tarea.texto}
+            <button onClick={() => marcarComoHecha(index)}>
+              {tarea.completada ? "❌" : "✅"}
+            </button>
+            <button onClick={() => eliminarTarea(index)}>❌</button>
+            </li>
           ))}
+          
         </ol>
        </form>
+       <button onClick={eliminarTodasLasTareas}>Eliminar Todas las Tareas</button>
     
     </div>
   )
